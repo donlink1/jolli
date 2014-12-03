aa70b67b87f4bebafc5b6b24da64fdee42e8d70f
Clay Miller <clay@smockle.com>
2014-12-03 02:17:24 -0600

    Fix FAQs by replacing junk metamorphs with unbound id property. Correctly add avatar.js and bootstrap.js in Brocfile.

--- Brocfile.js
+++ Brocfile.js
@@ -19,6 +19,9 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import("bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js");
+app.import("vendor/avatar.js");
+
 var tree = app.toTree();
 
 var fonts = pickFiles('bower_components/font-awesome/fonts', {
