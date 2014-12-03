8cfa96df06fe343da8e00909d04b378145523ba1
Clay Miller <clay@smockle.com>
2014-12-02 23:49:03 -0600

    Remove unused refs to broccoli-funnel.

--- Brocfile.js
+++ Brocfile.js
@@ -1,9 +1,8 @@
-/* global require, module, __dirname */
+/* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var mergeTrees = require('broccoli-merge-trees');
 var pickFiles = require('broccoli-static-compiler');
-var Funnel = require('broccoli-funnel');
 
 var app = new EmberApp();
 
@@ -27,11 +26,5 @@ var fonts = pickFiles('bower_components/font-awesome/fonts', {
   files: ['fontawesome-webfont.eot','fontawesome-webfont.ttf','fontawesome-webfont.svg','fontawesome-webfont.woff'],
   destDir: '/assets/fonts'
 });
-
-// var fonts = new Funnel(__dirname, {
-//   srcDir: 'bower_components/font-awesome/fonts',
-//   include: ['fontawesome-webfont.eot','fontawesome-webfont.ttf','fontawesome-webfont.svg','fontawesome-webfont.woff'],
-//   destDir: '/assets/fonts'
-// });
 
 module.exports = mergeTrees([tree, fonts]);
